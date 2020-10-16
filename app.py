8e375e072dd9edd0418e20a79030cbd394e05d54
Scott Rehn <38218315+rabscuttle@users.noreply.github.com>
2020-10-15 19:15:10 -0500

    Edited app.py

--- app.py
+++ app.py
@@ -1,39 +1,48 @@
-# Import 
+##########
+# Import #
+##########
+
 import sqlalchemy
+from config import username, password
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-from flask import Flask, render_template, redirect
+from sqlalchemy import create_engine
+from flask import Flask, render_template, redirect, jsonify
 
-#################################################
-# Database Setup
-#################################################
+##################
+# Database Setup #
+##################
 
-# Create Engine
-#engine = create_engine("postgresql:///username:password@localhost/database")
+database_name = 'MNDistrictData'
+connection_string = f'postgresql://{username}:{password}@localhost:5432/{database_name}'
 
-# reflect an existing database into a new model
-#Base = automap_base()
-# reflect the tables
-#Base.prepare(engine, reflect=True)
+# Connect to the database
+engine = create_engine(connection_string)
+base = automap_base()
+base.prepare(engine, reflect=True)
 
-# Save reference to the table
-#Passenger = Base.classes.passenger # Example
+# Select the tables
+district = base.classes.district
+incident = base.classes.incident
+incident_by_gender = base.classes.incident_by_gender
+incident_by_grade = base.classes.incident_by_grade
+incident_by_race = base.classes.incident_by_race
 
-#################################################
-# Flask Setup
-#################################################
+###############
+# Flask Setup #
+###############
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 # Effectively disables page caching
 
-#################################################
-# Flask Routes
-#################################################
+################
+# Flask Routes #
+################
 
 @app.route("/")
 def index():
     return render_template("index.html")
  
 
-
+# This statement is required for Flask
 if __name__ == '__main__':
     app.run(debug=True)
